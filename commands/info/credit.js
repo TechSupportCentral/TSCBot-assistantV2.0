e2143c5a474325162e994686bcbff2bb595a95e6
Philipp-spec <78240983+Philipp-spec@users.noreply.github.com>
2022-12-17 20:17:40 +0100

    Credits Update V171222

--- commands/info/credit.js
+++ commands/info/credit.js
@@ -12,7 +12,7 @@ module.exports = {
                 author: { name: "credits" },
                 fields: [
                    {name: "Main bot (Python)", value: "by BenTheTechGuy"},
-                   {name: "Assistant Bot (Javascript); handles staff activity tracking, credits", value: "by Philipp" },
+                   {name: "Assistant Bot (Javascript); handles staff activity tracking, credits", value: "by Philipp & BenTheTechGuy" },
                    {name: "Bot hosting", value: "by BenTheTechGuy"}
                 ],
         }
